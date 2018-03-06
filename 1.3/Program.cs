3bc60dd4185d799c2fa2911b5558c7a11e75b488
Danil Kulminskiy <iorsadg@gmail.com>
2018-03-06 12:43:49 +0300

    Add files via upload

---
+++ 1.3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Problem 1.3");
+            Console.WriteLine();
+            int[] arr = new int[50];
+            Random rnd = new Random();
+            Console.WriteLine("Before sort: ");
+            for (int i = 0; i < 50; i++)
+            {
+                arr[i] = rnd.Next() % 100;
+            }
+            Show(arr);
+            for (int i = 0; i < 49; i++)
+            {
+                int j = i;
+                while ((j >= 0) && (arr[j + 1] < arr[j]))
+                {
+                    int tmp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = tmp;
+                    j--;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("After Sort: ");
+            Show(arr);
+            Console.ReadKey();
+        }
+        static void Show(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+                Console.Write(' ');
+            }
+        }
+
+    }
+}
